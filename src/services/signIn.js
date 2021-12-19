cf7c65382c091475e20214fcaa890208a72556cc
Caio <lcaio1281@gmail.com>
2021-12-19 20:19:09 -0300

    add singIn service

---
+++ src/services/signIn.js
@@ -0,0 +1,15 @@
+import api from './api';
+
+// TODO:
+export const postSignIn = ({username, password}) => {
+  const apiBody = {
+    username,
+    password,
+  };
+
+  api
+    .post('sign-in', apiBody)
+    .then(res => JSON.stringify(res.data))
+    .then(data => console.log('data =', data))
+    .catch(err => console.error('[error] ', err));
+};
