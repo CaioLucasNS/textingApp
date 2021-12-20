bb2498c7794b16290cab731692836541f1a7c79c
Caio <lcaio1281@gmail.com>
2021-12-19 21:12:27 -0300

    add global styles

--- src/services/signIn.js
+++ src/services/signIn.js
@@ -10,6 +10,6 @@ export const postSignIn = ({username, password}) => {
   api
     .post('sign-in', apiBody)
     .then(res => JSON.stringify(res.data))
-    .then(data => console.log('data =', data))
+    // .then(data => console.log('data =', data))
     .catch(err => console.error('[error] ', err));
 };
---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+const colors = {
+    containerPrimary: '#8a9d93',
+    containerSecondary: '#3f666c',
+    containerTertiary: '#364c52',
+
+    yellow: '#e4c460',
+    orange: '#c27d37'
+}
+
+export default colors;
