075bd8f043866a38c34b8ba9e8b6ea9f341dcffa
Caio <lcaio1281@gmail.com>
2021-12-21 22:44:20 -0300

    fix getForgotPassword

--- src/services/getForgotPassword.js
+++ src/services/getForgotPassword.js
@@ -1,12 +1,19 @@
+import { Alert } from 'react-native';
 import api from './api';
 
 export const getForgotPassword = async ({username}) => {
+  const url = `forgot-password/${username}`;
+
   try {
-    await api
-      .get(`forgot-password/${username}`)
-      .then(res => JSON.stringify(res.data))
-      .then(data => console.log('senha =', data))
+    const response = await api.get(url)
+      .then(res => res)
       .catch(err => console.error('[error] ', err));
+
+      if (response.status == 200) {
+        return response.data;
+      } else {
+        Alert.alert('Erro ao solicitar sua senha', 'informe o nome de usuário correto!')
+      }
   } catch (error) {
     console.error('[ERROR] ', error);
     return Promise.reject(error);
